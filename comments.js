c2984484c55282f86b6ee8524560ac22575e8d0a
Mauricio Menezes <mauriciosm95@gmail.com>
2024-04-22 01:55:08 +0000

    fix third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,41 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server that listens on port 3000
+// When a request is made to the root of the server, send a file called index.html
+// When a request is made to the /comments path, send the contents of the comments.json file
 
-// create a route for comments
-app.get('/', (req, res) => {
-  res.send("Olá, mundo!");
-});
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// start the server
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+http.createServer((req, res) => {
+  console.log('Request received:', req.url);
+  if (req.url === '/comments') {
+    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.log('Error reading file:', err);
+        res.statusCode = 500;
+        res.end();
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(data);
+    }
+    );
+  }
+  else {
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+      if (err) {
+        console.log('Error reading file:', err);
+        res.statusCode = 500;
+        res.end();
+      }
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+    );
+  }
+}
+).listen(3000, () => {
+  console.log('Server listening on port 3000');
+}
+);
+
+// Create web server that listens on port 3000
