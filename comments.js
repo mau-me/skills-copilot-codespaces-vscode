5b5ae0001ceaf78f393c9e8fc1cafc4405422109
Mauricio Menezes <mauriciosm95@gmail.com>
2024-04-22 01:46:02 +0000

    fix third commit

--- comments.js
+++ comments.js
@@ -3,26 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// import the comments module
-const comments = require('./comments.js');
-
 // create a route for comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
+app.get('/', (req, res) => {
+  res.send("Olá, mundo!");
 });
 
 // start the server
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
-
-// Path: comments.js
-// create an array of comments
-const comments = [
-  { username: 'Tammy', comment: 'lololol' },
-  { username: 'FishBoi', comment: 'soooo funny' },
-  { username: 'QueenOfCats', comment: 'rofl' }
-];
-
-// export the comments array
-module.exports = comments;
